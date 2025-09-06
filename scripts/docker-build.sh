45bb54a66b44f82e23e9bc4a9fa9a61e93aa1a4f
Raúl Kuzner <raul.kuzner@mercadolibre.com>
2025-09-05 23:49:39 -0300

    update imageVersion

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -2,7 +2,7 @@
 
 dockerUserName="rkuzner"
 imageName="docker-qpdf"
-imageVersion="0.1.1"
+imageVersion="0.1.2"
 platformCodeList="amd64 arm64"
 
 # to run this commands, you should be logged to docker-hub!

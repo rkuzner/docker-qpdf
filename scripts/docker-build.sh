5bb32cf19a905c7a7e2f1b9631bee5edcacc233c
Raúl Kuzner <raul.kuzner@mercadolibre.com>
2025-10-14 18:01:29 -0300

    bump version number

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -2,7 +2,7 @@
 
 dockerUserName="rkuzner"
 imageName="docker-qpdf"
-imageVersion="0.1.5"
+imageVersion="0.1.6"
 platformCodeList="amd64 arm64"
 
 # to run this commands, you should be logged to docker-hub!
